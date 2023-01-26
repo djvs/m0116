b2c104337964df236a76f4f2949bc5c050e48f9d
djvs <djvs@users.noreply.github.com>
2023-01-26 12:35:14 -0500

    updates

--- outputs/gerber/jlcpcb_cpl.sh
+++ outputs/gerber/jlcpcb_cpl.sh
@@ -8,8 +8,9 @@ perl -pi -e 's/","[^"]*","[^"]*"/"/' jlcpcb-cpl*.csv
 
 
 # manual placement fix
-perl -pi -e 's/^"U3",(.*),180/"U3",\1,90/' jlcpcb-cpl*.csv
-perl -pi -e 's/^"U4",(.*),180/"U4",\1,90/' jlcpcb-cpl*.csv
+perl -pi -e 's/^"U3",(.*),180/"U3",\1,-90/' jlcpcb-cpl*.csv
+perl -pi -e 's/^"U4",(.*),180/"U4",\1,-90/' jlcpcb-cpl*.csv
+perl -pi -e 's/^"U5",(.*),90/"U5",\1,-90/' jlcpcb-cpl*.csv
 perl -pi -e 's/^"Y1",(.*),0.000000/"Y1",\1,180.000000/' jlcpcb-cpl*.csv
 perl -pi -e 's/^"Q4",(.*),-90/"Q4",\1,90/' jlcpcb-cpl*.csv
 
